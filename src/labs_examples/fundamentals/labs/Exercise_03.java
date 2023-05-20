0d6fadfd5a20568d9fbd0f205d1268f316439fff
David Bremner <davidpbremner@gmail.com>
2023-05-20 13:35:21 +0200

    Completed Exercise_03

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -2,12 +2,11 @@ package labs_examples.fundamentals.labs;
 
 /**
  * Fundamentals Exercise 3: My First Program
- *
- *      Write the necessary code to display the follow message to the console
- *
- *      Hello World!
- *      Check out my first program!
- *
+ * <p>
+ * Write the necessary code to display the follow message to the console
+ * <p>
+ * Hello World!
+ * Check out my first program!
  */
 
 public class Exercise_03 {
@@ -17,7 +16,7 @@ public class Exercise_03 {
         System.out.println("Hello World!");
 
         // write code to print out "Check out my first program!" below
-
+        System.out.println("Check out my first program!");
     }
 
 }
