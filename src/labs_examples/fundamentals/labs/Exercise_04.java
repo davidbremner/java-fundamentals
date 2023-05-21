9e9400a4fabb06d86b36ad7e6455f09c1eac07ac
David Bremner <davidpbremner@gmail.com>
2023-05-21 19:02:56 +0200

    Completed Exercise_04 and Exercise_05

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,7 +16,26 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        short primDataType2 = 20000;
+        System.out.println("Primitive data type short: " + primDataType2);
 
+        byte primDataType3 = 127;
+        System.out.println("Primitive data type byte: " + primDataType3);
+
+        long primDataType4 = 1232346798888989567L;
+        System.out.println("Primitive data type long: " + primDataType4);
+
+        char primDataType5 = 'd';
+        System.out.println("Primitive data type char: " + primDataType5);
+
+        boolean primDataType6 = true;
+        System.out.println("Primitive data type boolean: " + primDataType6);
+
+        float primDataType7 = 3848348938498394893483493483948938490.76786f;
+        System.out.println("Primitive data type float: " + primDataType7);
+
+        double primDataType8 = 12399999999999999999994.34987788887878895667543256;
+        System.out.println("Primitive data type double: " + primDataType8);
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,15 +14,25 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int lengthOfString = str.length();
+        System.out.println(lengthOfString);
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean comparison = str.equalsIgnoreCase(str2);
+        System.out.println(comparison);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String str3 = str + str2;
+        System.out.println(str3);
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        String updatedStr = str.replace("e", "o");
+        System.out.println(updatedStr);
 
+        String updatedStr2 = str2.toUpperCase();
+        System.out.println(updatedStr2);
     }
 
 
