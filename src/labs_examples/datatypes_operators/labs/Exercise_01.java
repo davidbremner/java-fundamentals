c8b758bf5f351bd36f41e422c706af2a20ed9220
David Bremner <davidpbremner@gmail.com>
2023-05-24 08:03:33 +0200

    Completed Exercise_01 in datatypes_operators lab

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -17,7 +17,14 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
-
+        int exampleOne = 12;
+        double exampleTwo = 17.3456765432123456543234554;
+        float exampleThree = 22.5456908765567899876578987656898765678f;
+        char exampleFour = 'b';
+        System.out.println(exampleOne);
+        System.out.println(exampleTwo);
+        System.out.println(exampleThree);
+        System.out.println(exampleFour);
     }
 }
 
