c4dc4fc3f537dc6200648b204d82f3738160e2d8
David Bremner <davidpbremner@gmail.com>
2023-11-17 11:27:54 +0100

    finished DataStructures custom LinkLists labs

--- src/labs_examples/datastructures/linkedlist/labs/Exercise_02.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_02.java
@@ -2,7 +2,6 @@ package labs_examples.datastructures.linkedlist.labs;
 
 /**
  *      LinkedLists - Exercise_02
- *
  *      Write your own custom LinkedList class. Although it can mimic the CustomLinkedList
  *      class we used here, it must be entirely unique. Please also add at least two
  *      additional helper methods that you think could be useful. Refer to Java's built-in
@@ -11,3 +10,109 @@ package labs_examples.datastructures.linkedlist.labs;
  *      Also, instead of using the index in the get() and remove() methods, these methods should
  *      get() and remove() based by the Node's value, not it's index.
  */
+
+class Exercise_02 {
+    public static void main(String[] args){
+        DavesLinkedList<Integer> myList = new DavesLinkedList<>(1);
+        myList.addLast(2);
+        myList.addLast(3);
+        myList.addFirst(0);
+        myList.addFirst(-1);
+        System.out.println(myList.contains(99));
+        myList.removeLast();
+        myList.removeFirst();
+        myList.clear(100);
+        myList.clear(10);
+        System.out.println(myList.contains(10));
+
+    }
+}
+
+class Node<E> {
+    E data;
+    Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+    public Node(){}
+}
+
+class DavesLinkedList<E> {
+    Node<E> head;
+    public DavesLinkedList(E data) {
+        this.head = new Node<>(data);
+    }
+
+    /**
+     * Adds a new Node to the end of the list.
+     * Process: Iterates through the LinkedList till it reaches the last linked Node class.
+     * @param data the data for the new node
+     */
+    void addLast(E data) {
+        Node<E> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node<>(data);
+    }
+
+    /**
+     * Adds a new Node to the beginning of the list.
+     * Process: Replaces the head Node with the newly created Node, linking this to the previous head Node.
+     * @param data the data for the new node
+     */
+    void addFirst(E data) {
+        head = new Node<>(data, head);
+    }
+
+    /**
+     * Removes a Node from the end of the list.
+     * Process: Iterates through the list checking the next Node's next variable to check if it is null and therefore
+     * the last Node of the list, consequently replacing the penultimate Node's next variable with null.
+     */
+    void removeLast() {
+        Node<E> current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+
+    /**
+     * Removes a Node from the start of the list.
+     * Process: Replaces the current head with the next Node in the list.
+     */
+    void removeFirst() {
+        head = head.next;
+    }
+
+    /**
+     * Clears the old list and begins a new one!
+     * @param data data for the head of the new list
+     */
+    void clear(E data) {
+        head = new Node<>(data);
+    }
+
+    /**
+     * Takes input of data and checks to see if it is in the list.
+     * Process: Iterates through the list while checking if the current Nodes data variable equals that of the data
+     * variable that was passed to the method.
+     * @param data the data to check if it is in the list.
+     * @return boolean of true or false if the data passed to the method is found in one of the Nodes.
+     */
+    boolean contains(E data) {
+        Node<E> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) return true;
+            if (current.next == null) return false;
+            current = current.next;
+        }
+        return false;
+    }
+}
--- src/labs_examples/datastructures/linkedlist/labs/Exercise_03.java
+++ src/labs_examples/datastructures/linkedlist/labs/Exercise_03.java
@@ -5,3 +5,102 @@ package labs_examples.datastructures.linkedlist.labs;
  *
  *      Please create your own custom doubly-LinkedList from start to finish :) You got this!
  */
+
+class Exercise_03 {
+    public static void main(String[] args){
+        LinkedListDoubly<Integer> myDoublyList = new LinkedListDoubly<>(1);
+        myDoublyList.addLast(3);
+        myDoublyList.removeFirst();
+        myDoublyList.addFirst(2);
+        myDoublyList.addFirst(1);
+        myDoublyList.removeLast();
+        System.out.println("Contains 2? " + myDoublyList.contains(5));
+        myDoublyList.clear(-1);
+
+    }
+}
+
+class LinkedListDoubly<E> {
+    NodeDoubly<E> head;
+    public LinkedListDoubly(E data) {
+        this.head = new NodeDoubly<>(data);
+    }
+
+    void removeFirst() {
+        head = head.next;
+        head.previous = null;
+    }
+
+    /**
+     * Removes a Node from the end of the list.
+     * Process: Iterates through the list checking the next Node's next variable to check if it is null and therefore
+     * the last Node of the list, consequently replacing the penultimate Node's next variable with null.
+     */
+    void removeLast() {
+        NodeDoubly<E> current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+    }
+
+    void addFirst(E data) {
+        head = new NodeDoubly<>(head, data);
+        head.next.previous = head;
+    }
+
+    void addLast(E data) {
+        NodeDoubly<E> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new NodeDoubly<>(data, current);
+    }
+
+    /**
+     * Clears the old list and begins a new one!
+     * @param data data for the head of the new list
+     */
+    void clear(E data) {
+        head = new NodeDoubly<>(data);
+    }
+
+    /**
+     * Takes input of data and checks to see if it is in the list.
+     * Process: Iterates through the list while checking if the current Nodes data variable equals that of the data
+     * variable that was passed to the method.
+     * @param data the data to check if it is in the list.
+     * @return boolean of true or false if the data passed to the method is found in one of the Nodes.
+     */
+    boolean contains(E data) {
+        NodeDoubly<E> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) return true;
+            if (current.next == null) return false;
+            current = current.next;
+        }
+        return false;
+    }
+}
+
+class NodeDoubly<E> {
+    E data;
+    NodeDoubly<E> next;
+    NodeDoubly<E> previous;
+    public NodeDoubly(E data) {
+        this.data = data;
+    }
+    public NodeDoubly(NodeDoubly<E> next, E data) {
+        this.next = next;
+        this.data = data;
+    }
+    public NodeDoubly(E data, NodeDoubly<E> previous) {
+        this.data = data;
+        this.previous = previous;
+    }
+    public NodeDoubly(E data, NodeDoubly<E> next, NodeDoubly<E> previous) {
+        this.data = data;
+        this.previous = previous;
+        this.next = next;
+    }
+}
