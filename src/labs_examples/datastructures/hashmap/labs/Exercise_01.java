58c203f7f23bec1c557f4533055ee26a8737938c
David Bremner <davidpbremner@gmail.com>
2024-03-08 08:41:28 +0100

    finished labs hashmaps

--- src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_01.java
@@ -1,5 +1,9 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *  HashMaps Exercise_01
  *
@@ -21,3 +25,40 @@ package labs_examples.datastructures.hashmap.labs;
  *  clear()
  *
  */
+
+class Exercise_01 {
+    public static void main(String[] args){
+        HashMap<String, String> newcastleUnitedResults = new HashMap<>();
+        HashMap<String, String> someEmptyList = new HashMap<>();
+        newcastleUnitedResults.put("Manchester United", "1-0");
+        newcastleUnitedResults.put("PSG", "1-1");
+        newcastleUnitedResults.put("Chelsea", "4-1");
+        System.out.println("The result against MUFC: " + newcastleUnitedResults.get("Manchester United"));
+        someEmptyList.putAll(newcastleUnitedResults);
+        System.out.println(someEmptyList);
+        System.out.println(newcastleUnitedResults);
+        System.out.println("Size of hashmap: " + newcastleUnitedResults.size());
+        System.out.println("Do we contain the key 'PSG'? " + newcastleUnitedResults.containsKey("PSG"));
+        System.out.println("Do we contain the value '4-1'? " + newcastleUnitedResults.containsValue("4-1"));
+
+        Set<String> myKeys = newcastleUnitedResults.keySet(); // not needed but an alternative way if required
+        int counter = 0;
+        for (String key : newcastleUnitedResults.keySet()) {
+            counter++;
+            System.out.println("Key " + counter + ": " + key);
+            System.out.println("Result (value): " + newcastleUnitedResults.get(key));
+        }
+
+        newcastleUnitedResults.putIfAbsent("Milan", "3-1");
+
+        Set<Map.Entry<String, String>> myEntrySets = newcastleUnitedResults.entrySet(); // also not needed but an alternative way if required
+        for (Map.Entry<String, String> entrySet : newcastleUnitedResults.entrySet()) {
+            System.out.println("Key value pair as an entrySet: " + entrySet.getKey() + " " + entrySet.getValue());
+        }
+
+        newcastleUnitedResults.remove("PSG");
+        newcastleUnitedResults.replace("Milan", "3-1", "4-0");
+        newcastleUnitedResults.forEach((key, value) -> System.out.println("Key: " + key + " Value: " + value));
+        newcastleUnitedResults.clear();
+    }
+}
--- src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
+++ src/labs_examples/datastructures/hashmap/labs/Exercise_03.java
@@ -1,5 +1,7 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.util.*;
+
 /**
  *      HashMaps Exercise_02
  *
@@ -19,3 +21,121 @@ package labs_examples.datastructures.hashmap.labs;
  *      3) search for 100 elements
  *      4) delete 100 elements
  */
+
+class Exercise_03 {
+    public static void main(String[] args){
+        int elementMax = 100;
+
+        // LinkedList
+        System.out.println("\n-----LINKEDLIST-----\n");
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        long startLinkedList = System.currentTimeMillis();
+        System.out.println(startLinkedList);
+        // 1) add 100 elements
+        for (int i = 0; i < elementMax; i++) {
+            linkedList.add(i + 1);
+        }
+        // 2) update 100 elements
+        for (int i = 0; i < elementMax; i++) {
+            linkedList.set(i, i + 2);
+        }
+        // 3) search for 100 elements
+        boolean found = false;
+        for (int i = 0; i < elementMax; i++) {
+            if (linkedList.get(i).equals(50)) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println("Was 50 found? " + found);
+        // 4) delete 100 elements
+        linkedList.clear();
+        long endLinkedList = System.currentTimeMillis();
+        System.out.println(endLinkedList);
+        System.out.println("Time taken in milliseconds: " + (endLinkedList - startLinkedList));
+
+        // Stack
+        System.out.println("\n-----STACK-----\n");
+        Stack<Integer> stack = new Stack<>();
+        long startStack = System.currentTimeMillis();
+        System.out.println(startStack);
+        // 1) add 100 elements
+        for (int i = 0; i < elementMax; i++) {
+            stack.add(i + 1);
+        }
+        // 2) update 100 elements
+        for (int i = 0; i < elementMax; i++) {
+            stack.set(i, i + 2);
+        }
+        // 3) search for 100 elements
+        boolean foundStack = false;
+        for (int i = 0; i < elementMax; i++) {
+            if (stack.get(i).equals(50)) {
+                foundStack = true;
+                break;
+            }
+        }
+        System.out.println("Was 50 found? " + foundStack);
+        // 4) delete 100 elements
+        stack.clear();
+        long endStack = System.currentTimeMillis();
+        System.out.println(endStack);
+        System.out.println("Time taken in milliseconds: " + (endStack - startStack));
+
+        // Queue
+        System.out.println("\n-----QUEUE-----\n");
+        Queue<Integer> queue = new LinkedList<>();
+        long startQueue = System.currentTimeMillis();
+        System.out.println(startQueue);
+        // 1) add 100 elements
+        for (int i = 0; i < elementMax; i++) {
+            queue.add(i + 1);
+        }
+        // 2) update 100 elements
+        Queue<Integer> newQueue = new LinkedList<>();
+        while (!queue.isEmpty()) {
+            int newValue = queue.poll() + 1;
+            newQueue.add(newValue);
+        }
+        // 3) search for 100 elements
+        boolean foundQueue = false;
+        for (int i = 0; i < elementMax; i++) {
+            if (newQueue.contains(50)) {
+                foundQueue = true;
+                break;
+            }
+        }
+        System.out.println("Was 50 found? " + foundQueue);
+        // 4) delete 100 elements
+        newQueue.clear();
+        long endQueue = System.currentTimeMillis();
+        System.out.println(endQueue);
+        System.out.println("Time taken in milliseconds: " + (endQueue - startQueue));
+
+        // HashMap
+        System.out.println("\n-----HASHMAP-----\n");
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        char asciiValue = 33;
+        long startHashMap = System.currentTimeMillis();
+        System.out.println(startHashMap);
+        // 1) add 100 elements
+        for (int i = 0; i < elementMax; i++) {
+            hashMap.put(String.valueOf(asciiValue), i + 1);
+            asciiValue++;
+        }
+        // 2) update 100 elements
+        asciiValue = 33;
+        for (int i = 0; i < elementMax; i++) {
+            hashMap.put(String.valueOf(asciiValue), i + 2);
+            asciiValue++;
+        }
+        // 3) search for 100 elements
+        boolean foundHash = hashMap.containsValue(50);
+        System.out.println("Was 50 found? " + foundHash);
+        // 4) delete 100 elements
+        hashMap.clear();
+        long endHash = System.currentTimeMillis();
+        System.out.println(endHash);
+        System.out.println("Time taken in milliseconds: " + (endHash - startHashMap));
+    }
+}
